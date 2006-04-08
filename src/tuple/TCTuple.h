5da47363dcb56f678c094ef15531ed8c060c3c4c
yglukhov <yglukhov@309b279a-6710-0410-b058-d98e029e4454>
2006-04-08 19:59:55 +0000

    Added tuples.
    git-svn-id: https://192.168.1.5/le/trunk@37 309b279a-6710-0410-b058-d98e029e4454

---
+++ src/tuple/TCTuple.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "TCUnitTuple.h"
+
+template <typename T, unsigned index>
+struct TSDefaultTupleUnit
+{
+	T mValue;
+};
+
+template <bool b>
+class Bool2Type
+{
+
+};
+
+template <class TTypeList>
+class TCTuple : public TCUnitTuple<TTypeList, TSDefaultTupleUnit>
+{
+	typedef TCUnitTuple<TTypeList, TSDefaultTupleUnit> Super;
+
+	public:
+		//////////////////////////////////////////////////////////////////////////
+		// value<index>() - return a reference to the object in the tuple at index
+		template <unsigned index>
+		typename TTypeList::TypeAt<index>::result& value()
+		{
+			return Super::template unit<index>().mValue;
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		// setValueNonStrict<index>(T) - if an object at index exists, set its
+		// value to T.
+		template <unsigned index, typename T>
+		inline void setValueNonStrict(const T& obj)
+		{
+			_setValueNonStrict<index, T>(obj, Bool2Type<(index >= TTypeList::length)>());
+		}
+
+	private:
+		template <unsigned i, typename T>
+		inline void _setValueNonStrict(const T& obj, Bool2Type<false>)
+		{
+			value<i>() = obj;
+		}
+
+		template<unsigned index, typename T>
+		inline void _setValueNonStrict(const T&, Bool2Type<true>)
+		{
+
+		}
+};
---
+++ src/tuple/TCUnitTuple.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "base/TSTuple.h"
+
+template <class TTypeList, template <typename, unsigned int> class TUnit>
+class TCUnitTuple
+{
+	TSTuple<typename TTypeList::list, TUnit> mTuple;
+
+	public:
+		template <unsigned int index>
+		inline TUnit<typename TTypeList::TypeAt<index>::result, index>& unit()
+		{
+			return static_cast<TUnit<typename TTypeList::TypeAt<index>::result, index>&>(mTuple);
+		}
+};
